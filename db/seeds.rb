705fe448afddf049026caa9d1630ae5efa21befe
Erik Brady <bookdoctor@gmail.com>
2015-09-19 08:40:48 -0500

    Routes established

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# def random_gpa
+#   (0.0..4.0).step(0.2).to_a.map { |n| n.round(2) }.sample
+# end
+
+# %w(Rails Javascript Design iOS Python Java .NET).each do |course_name|
+#   Course.create(name: course_name)
+# end
+
+# Course.all.each do |course|
+#   5.times do
+#     Student.create({
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       gpa: random_gpa,
+#       course_id: course.id
+#     })
+#   end
+# end
