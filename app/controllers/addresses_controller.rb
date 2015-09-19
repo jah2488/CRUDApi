80ffc35f9c5a24fce1aeaa248b30ff824ca1ece5
Erik Brady <bookdoctor@gmail.com>
2015-09-19 09:05:50 -0500

    Model dependencies updated

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,57 @@
+# class AddressesController < ApplicationController
+#   def index
+#     address = Address.all
+#     render json: address.to_json, status: 200
+#   end
+
+#   def new
+#     address = Address.new
+#     render json: address.to_json, status: 200
+#   end
+
+#   def show
+#     if Address.exists?(params[:id])
+#       address = Address.find(params[:id])
+#       render json: address.to_json, status: 200
+#     else
+#       render json: { error_msg: 'Address Not Found!', id: params[:id] }.to_json, status: 404
+#     end
+#   end
+
+
+  def create
+    address = Address.new
+    address.street_name = params[:street_name]
+    address.city        = params[:city]
+    address.state       = params[:state]
+    address.zip         = params[:zip]
+    address.save!
+    render json: address
+  end
+
+  def update
+    if Address.exists?(params[:id])
+      address             = Address.find(params[:id])
+      address.street_name = params.fetch(:street_name, address.street_name)
+      address.city        = params.fetch(:city, address.city)
+      address.state       = params.fetch(:state, address.state)
+      address.zip         = params.fetch(:zip, address.zip)
+      address.save!
+      render json: address, status: 200
+     else
+      render json: { error_msg: 'Address Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
+
+  def destroy
+    if Address.exists?(params[:id])
+      address = Address.find(params[:id])
+      address.destroy
+      message = "Address was deleted."
+      render json: message, status: 200
+    else
+      render json: { error_msg: 'Address Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,32 @@
 #     })
 #   end
 # end
+
+ 10.times do
+   user = User.create({
+     first_name:  Faker::Name.first_name,
+     last_name:   Faker::Name.last_name,
+     age:         Faker::Number(2),
+   })
+   Address.create({             #for each user(lowercase) assign an Address
+      user_id:     user.id,
+      street_name: Faker::Address.street_address,
+      city:        Faker::Address.city,
+      state:       Faker::Address.state,
+      zip:         Faker::Address.zip,
+    })
+  end
+
+
+  20.times do
+    item = Item.create({
+      name:     Faker::Commerce.product_name,
+      price:    Faker::Number.between(1, 345),
+      desc:     Faker::Commerce.color,
+      })
+    Order.create({
+      user_id:   Faker::Number.between(1,30),
+      item_id:   item.id,
+      qty:       Faker::Number.between(1,10),
+    })
+  end
