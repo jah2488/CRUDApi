0994fa67a83232d2fd0e0ffe1dc0cd6b5d10ef81
Erik Brady <bookdoctor@gmail.com>
2015-09-19 12:28:05 -0500

    Items controller written

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,51 +9,51 @@ class ItemsController < ApplicationController
     render json: items.to_json, status: 200
   end
 
-#   def show
-#     if Item.exists?(params[:id])
-#       item = item.find(params[:id])
-#       render json: item.to_json, status: 200
-#     else
-#       render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
-#     end
-#   end
+  def show
+    if Item.exists?(params[:id])
+      item = item.find(params[:id])
+      render json: item.to_json, status: 200
+    else
+      render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
 
-#   def users
-#     item = Item.find(params[:id])
-#     render json: item.users.to_json, status: 200
-#   end
+  def users
+    item = Item.find(params[:id])
+    render json: item.users.to_json, status: 200
+  end
 
 
-#   def create
-#     item = Item.new
-#     item.name    = params[:name]
-#     item.price   = params[:price]
-#     item.desc    = params[:desc]
-#     item.save!
-#     render json: item, status: 200
-#   end
+  def create
+    item = Item.new
+    item.name    = params[:name]
+    item.price   = params[:price]
+    item.desc    = params[:desc]
+    item.save!
+    render json: item, status: 200
+  end
 
-#   def update
-#     if Item.exists?(params[:id])
-#       item        = Item.find(params[:id])
-#       item.desc   = params.fetch(:desc, item.desc)
-#       item.name   = params.fetch(:name, item.name)
-#       item.price  = params.fetch(:price, item.price)
-#       item.save
-#       render json: item, status: 200
-#     else
-#       render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
-#     end
-#   end
+  def update
+    if Item.exists?(params[:id])
+      item        = Item.find(params[:id])
+      item.desc   = params.fetch(:desc, item.desc)
+      item.name   = params.fetch(:name, item.name)
+      item.price  = params.fetch(:price, item.price)
+      item.save
+      render json: item, status: 200
+    else
+      render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
 
-#   def destroy
-#     if Item.exists?(params[:id])
-#       item          = Item.find(params[:id])
-#       item.destroy
-#       message       = "Item was deleted."
-#       render json: message, status: 200
-#     else
-#       render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
-#     end
-#   end
-# end
+  def destroy
+    if Item.exists?(params[:id])
+      item          = Item.find(params[:id])
+      item.destroy
+      message       = "Item was deleted."
+      render json: message, status: 200
+    else
+      render json: { error_msg: 'Item Not Found!', id: params[:id] }.to_json, status: 404
+    end
+  end
+end
